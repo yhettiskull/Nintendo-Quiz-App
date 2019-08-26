bde11e5814b8bea37c3813a5dedbba0db2d9284f
YhettiSkull73 <nathanlloyd11@gmail.com>
2019-08-26 11:33:17 -0700

    Refactor
    refactor and condensation of code. Includes CSS refactor, new Questions Correct display, as well as new method for tracking user stats and updating.

--- Nintendo-Quiz-Data-Management.js
+++ Nintendo-Quiz-Data-Management.js
@@ -1,13 +1,12 @@
 const dataBank = [
     {
-        question: 'This plumber first saw audiences in 1981, before starring in his own game with his brother Luigi in 1983.',
+        question: 'This plumber, first saw audiences in 1981, before starring in his own game with his brother Luigi in 1983.',
         answers: ['Donkey Kong', 'Mario Kart', 'Mario', 'Wario'],
         correctAnswer: 'Mario',
         icon: `class='hidden'`,
         trueFalse: false,
         pointValue: 10,
-        userCorrect: '',
-        fact: 'Mario was originally just a no-name carpenter before his starlight role and new gig as a princess saving plumber in 1983.'
+        fact: 'Mario was originally just a no-name carpenter, before his starlight role and new gig as a princess saving plumber, in 1983.'
     },
     {
         question: 'Name this pokemon.',
@@ -17,7 +16,6 @@ const dataBank = [
         alt: 'Yellow Pokemon with red cheeks. Also known as the Electric Mouse Pokemon.',
         trueFalse: false,
         pointValue: 10,
-        userCorrect: '',
         fact: 'Pikachu is the most recognizable Pokemon from the long running cartoon & game franchise, but there are over 800 Pokemon now. Can you catch \'em all?'
     },
     {
@@ -28,57 +26,51 @@ const dataBank = [
         alt: 'A tall lanky character clad in purple overalls and an upside down L on his hat.',
         trueFalse: false,
         pointValue: 10,
-        userCorrect: '',
         fact: 'Waluigi, the long-time rival of Luigi, made his first apperence in Mario Tennis for the N64 in 2000.'
     },
     {
-        question: 'Named after its function and shape, this console well undersold after only 22 million units left shelves during its lifetime.',
+        question: 'Named after its function and shape, this console flopped after only 22 million units left shelves during its lifetime.',
         answers: ['GameBoy', 'Gamecube', 'Wii', 'GameSphere'],
         correctAnswer: 'Gamecube',
         icon: `class='hidden'`,
         trueFalse: false,
         pointValue: 20,
-        userCorrect: '',
-        fact: 'The Gamecube is considered one of Nintendo\'s largest failures. Though it had many now iconic titles, it fell flat compared to its competition including Sony\'s PlayStation 2.'
+        fact: 'The Gamecube is considered one of Nintendo\'s largest failures. Though it had many now iconic titles, it fell flat compared to its competition, including Sony\'s PlayStation 2.'
     },
     {
-        question: 'When the Wii debuted in 2006, which game came with all consoles?',
+        question: 'When the Wii debuted in 2006, which game came with each console?',
         answers: ['WiiFit', 'Mario Kart', 'Super Mario Bros 2', 'Wii Sports'],
         correctAnswer: 'Wii Sports',
         icon: `class='hidden'`,
         trueFalse: false,
         pointValue: 20,
-        userCorrect: '',
         fact: 'Wii Sports is not only the best selling video game of all time, but was the first game to truly showcase full motion based controls.'
     },
     {
-        question: 'Breaking past tradition, the Gamecube allowed players to play Wii games as long as players had the proprietary wireless Powerglove adapter.',
+        question: 'Breaking past tradition, the Gamecube allowed players to play Wii games, as long as players had the proprietary wireless Powerglove adapter.',
         answers: ['True', 'False'],
         correctAnswer: 'False',
         icon: `class='hidden'`,
         trueFalse: true,
         pointValue: 30,
-        userCorrect: '',
-        fact: 'The Gamecube spouted many excellent addons during its lifetime, including one of the first wireless controllers for console. The Powerglove however, performed terrible and was discontinued two years before the Gamecube was released.'
+        fact: 'The Gamecube spouted many excellent addons during its lifetime, including one of the first wireless controllers for console. The Powerglove however, performed terrible and was discontinued, two years before the Gamecube was released.'
     },
     {
-        question: 'Nintendos flagship handheld console the GameBoy met consumers for the first time in which year?',
+        question: 'Nintendo\'s flagship handheld console, the GameBoy, met consumers for the first time in which year?',
         answers: ['1989', '1997', '1990', '2000'],
         correctAnswer: '1989',
         icon: `class='hidden'`,
         trueFalse: false,
         pointValue: 30,
-        userCorrect: '',
-        fact: 'Arguably the most iconic console ever released for handheld, the GameBoy met shelves in 1989, and saw 6 models over 15 years.'
+        fact: 'Arguably the most iconic console ever released for handheld, the GameBoy met shelves in 1989, and saw 6 different models over 15 years.'
     },
     {
-        question: 'Bill Gates founded Nintendo in 1974 as a pet project. In 1980 after creating several fan-favorite arcade games he sold the company to a Japanese playing card company, and went on to create Microsoft and the Xbox console family.',
+        question: 'Bill Gates founded Nintendo in 1974 as a pet project. In 1980 after creating several fan-favorite arcade games, he sold the company to a Japanese playing card company, and went on to create Microsoft and the Xbox console family.',
         answers: ['True', 'False'],
         correctAnswer: 'False',
         icon: `class='hidden'`,
         trueFalse: true,
         pointValue: 30,
-        userCorrect: '',
         fact: 'Bill Gates founded Microsoft in 1974 to build computer parts. By the early 2000\'s Gates was clamoring to buy Nintendo, but they declined his many offers.'
     },
     {
@@ -88,8 +80,7 @@ const dataBank = [
         icon: `class='hidden'`,
         trueFalse: false,
         pointValue: 40,
-        userCorrect: '',
-        fact: 'The GameBoy Advance released in 2001 with a 2.9 inch display capable of 65,535 possible colors.'
+        fact: 'The GameBoy Advance released in 2001 with a 2.9 inch display, capable of 65,535 possible colors.'
     },
     {
         question: 'Nintendo was founded in which year?',
@@ -98,8 +89,7 @@ const dataBank = [
         icon: `class='hidden'`,
         trueFalse: false,
         pointValue: 40,
-        userCorrect: '',
-        fact: 'Nintendo started as a playing card company in Kyoto in 1889. Looking to expand they turned to toys in the 60\'s, finally leaping into video games in the 70\'s.'
+        fact: 'Nintendo started as a playing card company in Kyoto in 1889. Looking to expand, they turned to toys in the 60\'s, finally leaping into video games in the 70\'s.'
     },
 ];
 
